8b9196121d3a5669a1c3b63f14e15e14b697702d
sakai memoru <sakai.memoru@gmail.com>
2022-09-21 07:06:43 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+using Modules;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+      var name = "C# Replit";
+      var P = new Module();
+      P.DoMain(name);
+    }
+}
---
+++ modules/Module.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Modules
+{
+  class Module
+  {
+    public void DoMain(string str){
+      Console.WriteLine($"Hello, {str} !!!");
+    }
+    
+  }
+  
+}
