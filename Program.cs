360ad3fb0559210a195c7e1fa396fd2f2dde1039
sakai memoru <sakai.memoru@gmail.com>
2022-09-22 07:52:42 +0900

    Try a xunit.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
-using System;
+﻿using System;
 using Modules;
 
 class Program
 {
     public static void Main(string[] args)
     {
-      var name = "C# Replit";
-      var P = new Module();
-      P.DoMain(name);
+        var name = "C# Replit";
+        var P = new Module();
+        P.DoMain(name);
+
+        // sample
+        var i = 123;
+        var j = 456;
+        var result = Calc.Add(i, j);
+        Console.WriteLine($"result = {result}");
+
     }
 }
--- modules/Module.cs
+++ modules/Module.cs
@@ -2,12 +2,24 @@ using System;
 
 namespace Modules
 {
-  class Module
-  {
-    public void DoMain(string str){
-      Console.WriteLine($"Hello, {str} !!!");
-    }
+    class Module
+    {
+        public void DoMain(string str)
+        {
+            Console.WriteLine($"Hello, {str} !!");
+        }
     
-  }
+    }
   
+    class Calc
+    {
+        public static int Add(int x, int y) 
+        {
+            int ret = 0;
+            ret = x + y;
+            return ret;
+        }
+
+    }
+
 }
